fb3a09e2f54be09801f1d92e7e2f77f9a6956c4a
Emmanuel Ayeniko <108879983+NIKO-DEV06@users.noreply.github.com>
2025-07-12 21:37:03 +0100

    WIP: payments

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,5 +1,14 @@
 import { Type } from 'class-transformer';
-import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsDate,
+  IsDefined,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+import { CreateChargeDto } from '@app/common/dto/create-charge-dto';
 
 export class CreateReservationDto {
   @IsDate()
@@ -17,4 +26,9 @@ export class CreateReservationDto {
   @IsString()
   @IsNotEmpty()
   invoiceId: string;
+
+  @IsDefined()
+  @IsNotEmptyObject()
+  @ValidateNested()
+  charge: CreateChargeDto;
 }
